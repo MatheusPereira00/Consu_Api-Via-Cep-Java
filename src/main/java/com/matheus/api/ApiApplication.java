5f2612d999741ce60100c29d79f4dfd5add47f50
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-10-30 09:45:16 -0300

    feat: finish controller

--- src/main/java/com/matheus/api/ApiApplication.java
+++ src/main/java/com/matheus/api/ApiApplication.java
@@ -1,13 +1,17 @@
 package com.matheus.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		RestTemplate restTemplate = new RestTemplate();
+
+		String response = restTemplate.getForObject("https://dummyjson.com/products", String.class);
+
+		System.out.println(response);
 	}
 
 }
